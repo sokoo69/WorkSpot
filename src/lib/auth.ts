cd007fa3a8ac86ffec315b33ee0280413de199e0
saykot biswas shawon <shawon.saykot2023@gmail.com>
2026-07-14 13:21:41 +0600

    setup better auth authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,48 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }
+  },
+  plugins: [jwt()],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "user",
+      },
+      favorites: {
+        type: "string[]",
+        required: false,
+        defaultValue: [],
+      },
+      phoneNumber: {
+        type: "string",
+        required: false,
+      },
+      isVerifiedOwner: {
+        type: "boolean",
+        required: false,
+        defaultValue: false,
+      },
+    },
+  },
+});
