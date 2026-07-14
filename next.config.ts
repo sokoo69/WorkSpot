01c6b9cb1a9f60e3eb5cb0658bd4c223bdfd06be
saykot biswas shawon <shawon.saykot2023@gmail.com>
2026-07-14 13:21:41 +0600

    update next configuration and package dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
